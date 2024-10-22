a65a665109f055e4a314cff7f1862a843d1e1fe3
aron211 <aronsandoval16@gmail.com>
2024-10-22 14:05:45 -0400

    add new funtion to search

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query  } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { CreateInventoryDto } from './dto/create-inventory.dto';
 import { UpdateInventoryDto } from './dto/update-inventory.dto';
@@ -6,7 +6,7 @@ import { UpdateInventoryDto } from './dto/update-inventory.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { Role } from 'src/common/enums/rol.enum';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
-
+import { Inventory } from './entities/inventory.entity';
 @ApiTags('inventory')
 @Controller('inventory')
 export class InventoryController {
@@ -17,18 +17,24 @@ export class InventoryController {
     return this.inventoryService.create(createInventoryDto);
   }
 
-  @ApiBearerAuth()
+  // @ApiBearerAuth()
   @Get()
-  @Auth(Role.ADMIN,Role.USER,Role.TECNICHAL)
+  // @Auth(Role.ADMIN,Role.USER,Role.TECNICHAL)
   findAll() {
     return this.inventoryService.findAll();
   }
+  @Get('search')
+  async searchInventory(@Query('keywords') keywords: string): Promise<Inventory[]> {
+      console.log('Keywords:', keywords);
+      const keywordArray = keywords.split(' '); // Divide las palabras clave
+      return await this.inventoryService.searchInventory(keywordArray);
+  }
+  
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.inventoryService.findOne(id);
   }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateInventoryDto: UpdateInventoryDto) {
     return this.inventoryService.update(id, updateInventoryDto);
--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,13 +1,11 @@
-import { Injectable,
-  BadRequestException,
-  UnauthorizedException,
- } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, In } from 'typeorm';
 import { Inventory } from './entities/inventory.entity';
 import { CreateInventoryDto } from './dto/create-inventory.dto';
 import { UpdateInventoryDto } from './dto/update-inventory.dto';
 import { ConfigService } from '@nestjs/config';
+import { Like } from 'typeorm';
 
 @Injectable()
 export class InventoryService {
@@ -15,7 +13,7 @@ export class InventoryService {
     @InjectRepository(Inventory)
     private readonly InventoryRepository: Repository<Inventory>,
     private readonly configService: ConfigService,
-  ) { }
+  ) {}
 
   create(createInventoryDto: CreateInventoryDto) {
     return this.InventoryRepository.save(createInventoryDto);
@@ -24,15 +22,41 @@ export class InventoryService {
   async findAll() {
     return await this.InventoryRepository.find();
   }
+  async searchInventory(keywords: string[]): Promise<Inventory[]> {
+    try {
+      // Crear condiciones de búsqueda con ILike
+      const queryBuilder = this.InventoryRepository.createQueryBuilder('inventory');
+  
+      // Comprobar que hay palabras clave para buscar
+      if (keywords.length > 0) {
+        // Inicializar la condición de búsqueda
+        queryBuilder.where('1=1'); // Condición verdadera para facilitar el uso de AND
+  
+        // Agregar condiciones AND para cada palabra clave
+        keywords.forEach((keyword) => {
+          queryBuilder.andWhere('inventory.name ILIKE :keyword', { keyword: `%${keyword}%` });
+        });
+      }
+  
+      // Ejecutar la consulta
+      const results = await queryBuilder.getMany();
+      console.log('Search Results:', results); // Log para verificar los resultados
+      return results;
+    } catch (error) {
+      console.error('Error en searchInventory:', error);
+      throw new BadRequestException('Error al realizar la búsqueda.');
+    }
+  }
 
   findOne(id: string) {
     return this.InventoryRepository.findOneBy({ id });
   }
 
-    // Método para encontrar inventarios por múltiples IDs
-    async findByIds(ids: string[]): Promise<Inventory[]> {
-      return this.InventoryRepository.findBy({ id: In(ids) });
-    }
+  // Método para encontrar inventarios por múltiples IDs
+  async findByIds(ids: string[]): Promise<Inventory[]> {
+    return this.InventoryRepository.findBy({ id: In(ids) });
+  }
+
   findOneById(id: string) {
     return this.InventoryRepository.findOne({ 
       where: { id },
@@ -46,7 +70,7 @@ export class InventoryService {
         'cost',
         'sucursal',
       ],
-     });
+    });
   }
 
   async update(id: string, updateInventoryDto: UpdateInventoryDto) {
@@ -56,7 +80,6 @@ export class InventoryService {
     }
     return await this.InventoryRepository.update(id, { ...updateInventoryDto });
   }
-
 
   async remove(id: string) {
     await this.findOne(id);
